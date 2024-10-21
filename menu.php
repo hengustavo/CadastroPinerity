6018c916d7ef1af0810cb0581d56e809ae5b6a82
ItsNiSantana <s.nicoli@aluno.ifsp.edu.br>
2024-10-21 10:21:22 -0300

    hihi

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <div>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="CadastroBeneficiario">Cadastrar Beneficiário</a></li>
+            <li><a href="CadastroBeneficiario.php">Cadastrar Beneficiário</a></li>
             <li><a href="ListarCidade.php">Listar Pessoa </a></li>
             <li><a href="CadastroCliente.php">Cadastrar Cliente</a></li>
             <li><a href="ListarCliente.php">Listar Cliente</a></li>
